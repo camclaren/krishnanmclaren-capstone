77d25251edaf91b6fadefb048966806759ee4544
camclaren <cmclaren@tulane.edu>
2025-02-07 16:20:12 -0600

    Random term generator for test page
    hard-coded now, will need to fix later

---
+++ asl-website/test.js
@@ -0,0 +1,23 @@
+const terms = [
+    'ARM',
+    'BACK',
+    'CHEST',
+    'EARS',
+    'EYES',
+    'HEAD',
+    'JAW',
+    'KNEES',
+    'NOSE',
+    'RIBS',
+    'STOMACH',
+    'THROAT'
+];
+
+function generateTerm() {
+    const randomIndex = Math.floor(Math.random() * terms.length);
+    document.getElementById("term-display").innerText = terms[randomIndex];
+}
+
+function goToGradio() {
+    window.location.href = "https://your-gradio-app-link.com"; // Change this to your Gradio app URL
+}
